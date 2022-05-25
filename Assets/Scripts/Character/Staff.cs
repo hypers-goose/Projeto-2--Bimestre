ac94d687cf7876f25d745741f03b3c4bd2fdf49b
deogos <diogo.alonso@etec.sp.gov.br>
2022-05-25 19:12:54 -0300

    "Menu" feito e conclusão do raycast

--- Assets/Scripts/Character/Staff.cs
+++ Assets/Scripts/Character/Staff.cs
@@ -22,6 +22,13 @@ public class Staff : MonoBehaviour
             if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
             {
                 Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
             }
         }
 }
