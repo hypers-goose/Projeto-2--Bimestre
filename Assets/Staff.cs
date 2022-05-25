72c2a0484240c5ed0da287f4f3a5275d81def5e0
deogos <diogo.alonso@etec.sp.gov.br>
2022-05-24 21:57:51 -0300

    Adição de assets e começo do script raycast
    -Adicionado um asset de um cajado
    - Script raycast criado

---
+++ Assets/Staff.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Staff : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"));
+        {
+            Shoot();
+        }
+
+        void Shoot();
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+
+}
